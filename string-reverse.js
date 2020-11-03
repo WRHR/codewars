e0252cf65c0392dbd9c3a8a700d9e9baaf9e4893
WRHR <wrhreeves@gmail.com>
2020-11-02 22:57:37 -0700

    Add memory performant function

--- string-reverse.js
+++ string-reverse.js
@@ -1,11 +1,21 @@
+// function reverse(str){
+//   let string = (typeof str !== 'string') ? str.toString() : str
+//   let reversed = ''
+//   for(let i = string.length - 1; i >= 0; i--){
+//     console.log(string[i])
+//     reversed += string[i]
+//   }
+//   return reversed
+//}
+
 function reverse(str){
   let string = (typeof str !== 'string') ? str.toString() : str
-  let reversed = ''
+  let reversed = []
   for(let i = string.length - 1; i >= 0; i--){
     console.log(string[i])
-    reversed += string[i]
+    reversed.push(string[i])
   }
-  return reversed
+  return reversed.join('')
 }
 
 console.log(reverse('abc'))
