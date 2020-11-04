d4f54041be2fcc32e16a12f24ed9af4b770b383c
WRHR <wrhreeves@gmail.com>
2020-11-03 23:37:27 -0700

    Chagne splice to slice

--- javascript/maskify.js
+++ javascript/maskify.js
@@ -1,7 +1,7 @@
 function maskify(cc) {
   if(cc.length < 4){ return cc }
   let char = cc.split('')
-  let lastFour = char.splice(char.length - 4, 4)
+  let lastFour = char.slice(char.length - 4)
   let chunk = char.slice(0, cc.length - 4).map(char => char = '#')
   return [...chunk, ...lastFour].join('')
 }
