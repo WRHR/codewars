f05af266a7e5c947251f7063f7ba7a6f4a0a4ecc
WRHR <wrhreeves@gmail.com>
2020-11-03 23:28:23 -0700

    Create maskify with solution

---
+++ javascript/maskify.js
@@ -0,0 +1,10 @@
+function maskify(cc) {
+  if(cc.length < 4){ return cc }
+  let char = cc.split('')
+  let lastFour = char.splice(char.length - 4, 4)
+  let chunk = char.slice(0, cc.length - 4).map(char => char = '#')
+  return [...chunk, ...lastFour].join('')
+}
+
+console.log(maskify('4556364607935616'))
+console.log(maskify('455'))
