a060bb1d3428e74f9a29a4c99e18c4058ea50083
WRHR <wrhreeves@gmail.com>
2020-11-08 20:58:58 -0700

    Format methods

--- javascript/anagrams.js
+++ javascript/anagrams.js
@@ -1,7 +1,12 @@
 function anagrams(word, words) {
   return words.filter(w => {
       if(w.length === word.length 
-        && w.split('').sort().join('').toLowerCase().includes(word.split('').sort().join('').toLowerCase())){
+        && w
+          .split('')
+          .sort()
+          .join('')
+          .toLowerCase()
+          .includes(word.split('').sort().join('').toLowerCase())){
           return w
       }
   })
