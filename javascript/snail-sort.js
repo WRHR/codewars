10bff23eda060da25d0e439adb71c2a262cbb2a8
WRHR <wrhreeves@gmail.com>
2020-11-11 13:17:34 -0700

    Add solution

--- javascript/snail-sort.js
+++ javascript/snail-sort.js
@@ -4,7 +4,17 @@ array = [[1,2,3],
 
 function snail(array) {
   let snailed = []
-  
+  while(array.length){
+    snailed.push(...array.shift())
+    for(let i = 0; i < array.length; i++){
+      snailed.push(array[i].pop())
+    }
+    snailed.push(...(array.pop() || []).reverse())
+    for(let i = array.length -1; i >=0; i--){
+      snailed.push(array[i].shift())
+    }
+  }
+  return snailed
 }
 
 console.log(snail(array))
