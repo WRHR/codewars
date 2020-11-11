3c24889a5c4bf1a87d984b8e925c4c61947e10c5
WRHR <wrhreeves@gmail.com>
2020-11-11 14:50:57 -0700

    Create tribonacci and add solution

---
+++ javascript/tribonacci.js
@@ -0,0 +1,19 @@
+function tribonacci(signature,n){
+  let sequence = [...signature]
+  if(n < 3){
+    let arr = []
+    for(let i =0; i < n; i++){
+      arr.push(signature[i])
+    }
+    return arr
+  }
+  let i = 0
+  while(sequence.length < n){
+    let next = sequence[i] + sequence[i+1] + sequence[i+2]
+    sequence.push(next)
+    i++
+  }
+  return sequence
+}
+
+console.log(tribonacci([1,1,1], 10))
