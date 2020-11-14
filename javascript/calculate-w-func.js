7e36d79b77e1d24bb390b91be7408bd834ab3d9b
WRHR <wrhreeves@gmail.com>
2020-11-13 20:20:37 -0700

    Add solution

--- javascript/calculate-w-func.js
+++ javascript/calculate-w-func.js
@@ -1,20 +1,40 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function expression(num, operation){
+  return !operation ? num : operation(num)
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function zero(operation) { return expression(0, operation)}
+function one(operation) { return expression(1, operation)}
+function two(operation) { return expression(2, operation)}
+function three(operation) { return expression(3, operation)}
+function four(operation) { return expression(4, operation)}
+function five(operation) { return expression(5, operation)}
+function six(operation) { return expression(6, operation)}
+function seven(operation) { return expression(7, operation)}
+function eight(operation) { return expression(8, operation)}
+function nine(operation) { return expression(9, operation)}
 
-Test.assertEquals(seven(times(five())), 35);
-Test.assertEquals(four(plus(nine())), 13);
-Test.assertEquals(eight(minus(three())), 5);
-Test.assertEquals(six(dividedBy(two())), 3);
+function plus(x) {
+  return function (y){
+    return x + y
+  }
+}
+function minus(x) {
+  return function (y) {
+    return y - x
+  }
+}
+function times(x) {
+  return function (y) {
+    return x * y
+  }
+}
+function dividedBy(x) {
+  return function(y){
+    return parseInt(y / x)
+  }
+}
+
+console.log(seven(times(five())));
+console.log(four(plus(nine())));
+console.log(eight(minus(three())));
+console.log(six(dividedBy(two())));
