db366e21f574bedd34e10b425a0ed6211bc00c65
WRHR <wrhreeves@gmail.com>
2020-11-14 23:06:16 -0700

    Solution using filter

--- javascript/array-diff.js
+++ javascript/array-diff.js
@@ -1,15 +1,15 @@
-// function arrayDiff(a, b) {
-//   return a.filter(val => (!b.includes(val)))
-// }
-
 function arrayDiff(a, b) {
-  let diffArr = []
-  a.forEach(val => {
-    if(!b.includes(val)){
-      diffArr.push(val)
-    }
-  })
-  return diffArr
+  return a.filter(val => val !== b[0])
 }
+
+// function arrayDiff(a, b) {
+//   let diffArr = []
+//   a.forEach(val => {
+//     if(!b.includes(val)){
+//       diffArr.push(val)
+//     }
+//   })
+//   return diffArr
+// }
 
 console.log(arrayDiff([-17,16,15,0,19,15,-17,15,-8,-17,-19],[-17]))
